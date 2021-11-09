81101aee2062657064f699d11e85c9e2d9636d43
mrzaremba <Lopo2006>
2021-11-09 14:05:57 -0800

    test

---
+++ LessonOne/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        //Primitive data types
+        /*
+        int
+        double
+        char
+        boolean
+         */
+        System.out.println(5.0);
+        System.out.println(Math.sqrt(Math.pow(2,10)) + 1);
+
+    }
+}
